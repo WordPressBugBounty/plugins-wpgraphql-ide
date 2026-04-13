28b9f7b14e2a2330fb2a1deded382c50b8c70942
Sean Murphy <sean@iamseanmurphy.com>
2026-04-13 22:27:29 +0000

    4.3.0

---
+++ wpgraphql-ide/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-graphql/wpgraphql-ide',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '11f4091cbd8cc3130e0e36a451fc7f353aaab321',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'v2.0.6',
+            'version' => '2.0.6.0',
+            'reference' => '2e553be6a47b387a7b05496f6a6e8d8595dd0d08',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wpgraphql-ide' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '11f4091cbd8cc3130e0e36a451fc7f353aaab321',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
